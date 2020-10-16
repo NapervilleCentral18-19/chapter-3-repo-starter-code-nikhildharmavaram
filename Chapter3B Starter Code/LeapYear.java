13f04c0075526f8a0cf3dd7e289b7019b4a08f4a
nikhildharmavaram <ndharmavaram@stu.naperville203.org>
2020-10-16 12:21:14 -0500

    leapyear

---
+++ Chapter3B Starter Code/LeapYear.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class LeapYear here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class LeapYear
+{
+    public static void main(String args[])
+    {
+       int year;
+       Scanner keyboard = new Scanner(System.in);
+       year = keyboard.nextInt();
+       if (year < 1582)
+        System.out.println("Sorry, not the current calendar year");
+       else
+       {
+          if (year % 100 = 0)
+            
+       }
+         
+         
+    }
+}
