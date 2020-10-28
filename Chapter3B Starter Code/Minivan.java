1f6bf8d7521b4e12fefcb6dee980f560569e16a7
nikhildharmavaram <ndharmavaram@stu.naperville203.org>
2020-10-28 13:22:18 -0500

    wrong

--- Chapter3B Starter Code/Minivan.java
+++ Chapter3B Starter Code/Minivan.java
@@ -22,8 +22,27 @@ public class Minivan
         }
         else if (switches.charAt(2) == '1' && switches.charAt(4) == '1' && switches.charAt(5) == '1')
         {
-            right = false;
-            left = false;
+           right = false;
+           left = false;
+        }
+        else if (switches.charAt(2) == '1' && (switches.charAt(0) == '1' || switches.charAt(6) == '1') && (switches.charAt(1) == '1' || switches.charAt(7) == '1'))
+        {
+          right = true;
+          left = true;
+        }
+        else if (switches.charAt(2) == '1' && ((switches.charAt(0) == '1' || switches.charAt(6) == '1') || (switches.charAt(1) == '1' || switches.charAt(7) == '1')))
+        
+        {
+            if (switches.charAt(0) == '1' || switches.charAt(6) == '1')
+            {
+                right = false;
+                left = true;
+            }
+            else
+            {
+                right = true;
+                left = false;
+            }
         }
         else if ((switches.charAt(0) == '1' && switches.charAt(1) == '1') || (switches.charAt(4) == '1' && switches.charAt(5) == '1') || (switches.charAt(6) == '1' && switches.charAt(7) == '1'))
         {
