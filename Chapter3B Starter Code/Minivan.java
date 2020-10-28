9897116407f7398920750c25c3740f32a56a899e
nikhildharmavaram <ndharmavaram@stu.naperville203.org>
2020-10-27 21:50:30 -0500

    Finished minivan

--- Chapter3B Starter Code/Minivan.java
+++ Chapter3B Starter Code/Minivan.java
@@ -20,5 +20,36 @@ public class Minivan
             right = false;
             left = false;
         }
+        else if (switches.charAt(2) == '1' && (switches.charAt(4) == '1' || switches.charAt(5) == '1'))
+        {
+            right = false;
+            left = false;
+        }
+        else if ((switches.charAt(0) == '1' && switches.charAt(1) == '1') || (switches.charAt(4) == '1' && switches.charAt(5) == '1') || (switches.charAt(6) == '1' && switches.charAt(7) == '1'))
+        {
+            right = true;
+            left = true;
+        }
+        else if (switches.charAt(0) == '1' || switches.charAt(4) == '1' || switches.charAt(6) == '1')
+        {
+            right = false;
+            left = true;
+        }
+        else
+        {
+            right = true;
+            left = false;
+        }
+        
+        if (right == false && left == false)
+            System.out.println("Neither door opens");
+        else if (right == true && left == false)
+            System.out.println("Right door opens");
+        else if (right == false && left == true)
+            System.out.println("Left door opens");
+        else
+            System.out.println("Both doors open");
+            
+        
     }
 }
